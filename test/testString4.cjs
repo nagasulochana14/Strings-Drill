f40b32477a9f18e6601bac76969678c3fbb3e6e3
sulochana <nagasulochanab53@gmail.com>
2024-02-26 11:05:12 +0530

    created for testString4.cjs

---
+++ test/testString4.cjs
@@ -0,0 +1,5 @@
+const string4 = require("../string4.cjs");
+
+const object = {"first_name": "JoHN", "middle_name": "doe", "last_name": "SMith"};
+const result = string4(object);
+console.log(result);
